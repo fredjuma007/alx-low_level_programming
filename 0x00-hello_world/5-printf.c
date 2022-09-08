1bee8893c03c62435299227fcae8213c74c09898
fredjuma007 <fredjuma8@gmail>
2022-09-08 11:20:36 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*Description: main-Prints out a string to stdout.
+ * Return: ZERO if success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
