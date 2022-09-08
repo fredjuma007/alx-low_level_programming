0ea72fb6dd124bef73d6bffe8ae1491c712275b5
fredjuma007 <fredjuma8@gmail>
2022-09-08 10:13:11 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * the function to use is 'puts'
+ * not allowed to use 'printf'
+ * should end with value ZERO
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
