088d0df903b60055355f5ad3e7ee37e63a4f7caa
fredjuma007 <fredjuma8@gmail>
2022-09-09 13:40:43 -0700

    zero to nine

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * Description: main - prints all base ten digits from 0 to 9
+ *
+ * Return: 0 if successful
+ *
+ */
+int main(void)
+{
+	printf ("0,1,2,3,4,5,6,7,8,9");
+		return (0);
+}
