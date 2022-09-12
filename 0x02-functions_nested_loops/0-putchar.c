e9e394e5d87ab58bde80d7fe30365b5424254bb0
fredjuma007 <fredjuma8@gmail>
2022-09-12 12:37:56 -0700

    putchar mess

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
-/*Description: main - printing _putchar and a new line
+/**
+ * Description: main - prints '_putchar' and a new line
  *
- * return: 0
+ * REturn: 0
+ *
  */
 
 int main(void)
 {
-	printf ("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
