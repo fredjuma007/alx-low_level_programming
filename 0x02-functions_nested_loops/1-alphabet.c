f1eb46a001b48c54a0e4b3a35268a609a728330e
fredjuma007 <fredjuma8@gmail>
2022-09-12 15:03:09 -0700

    alphabet in lower

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Main (check the code)- print alphabet in lowercase
+ *
+ * Return: 0 
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
