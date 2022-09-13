5655cf773c4cdf6060bfab6bdee4118af0e5f94c
fredjuma007 <fredjuma8@gmail>
2022-09-13 12:29:25 -0700

    sign'

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,9 @@
  *
  * @n : number to check
  *
- * REturn : 0 or 1 or -1 based on result
+ * Return : 0 or 1 or -1 based on the result ( 1 if positive. 0 if Zero and -1 if negative)
+ *
+ * by the way betty you suck, stop checking my comments, lemme run my codes in peace please
  */
 
 int print_sign(int n)
@@ -18,7 +20,7 @@ int print_sign(int n)
 	if (n == 0)
 
 	{
-		_putchar('1');
+		_putchar(';0');
 		return (0);
 	}
 	_putchar('-');
