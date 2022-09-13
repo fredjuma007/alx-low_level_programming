61bb25a74e6d15c670478de5e8a19fb82a43e9c7
fredjuma007 <fredjuma8@gmail>
2022-09-13 12:34:27 -0700

    fixed sign again, come on betty

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,7 +7,6 @@
  *
  * Return : 0 or 1 or -1 based on the result ( 1 if positive. 0 if Zero and -1 if negative)
  *
- * by the way betty you suck, stop checking my comments, lemme run my codes in peace please
  */
 
 int print_sign(int n)
