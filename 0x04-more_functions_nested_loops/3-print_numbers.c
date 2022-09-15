237cdc7ca999acd0d73046977304158205191d54
fredjuma007 <fredjuma8@gmail>
2022-09-15 13:08:17 -0700

    one plus one,

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+
+/**
+ * print_numbers - prints numbers
+ * Return:0
+ */
+
+void print_number(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num); } _putchar('\n');
+}
