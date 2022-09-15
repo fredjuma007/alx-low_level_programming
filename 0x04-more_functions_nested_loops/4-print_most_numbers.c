e44b6f8672f7d37db323ad43bcb979f56eb5b3b7
fredjuma007 <fredjuma8@gmail>
2022-09-15 13:46:50 -0700

    i like num as a variable dont judge me

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,13 +5,13 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int num;
 
-	for (i = '0'; i <= '9'; i++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		if (i != '2' &&  i != '4')
+		if (num != '2' &&  num != '4')
 		{
-			_putchar(i);
+			_putchar(num);
 		}
 	}
 	_putchar('\n');
