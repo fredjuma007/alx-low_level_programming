6978061c4233f7b7a38f5c2b9349f0aaf1f54c84
fredjuma007 <fredjuma8@gmail>
2022-09-15 17:09:25 -0700

    more lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints diagonal sh*t
+ *
+ * @n: integer to set diagonal
+ */
+
+void print_diagonal(int n)
+{
+	int x;
+	int y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}else
+	{
+		for (x = 0; x < n; x++)
+		{
+			for (y = 0; y < x; y++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
