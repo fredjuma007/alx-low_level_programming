06b51679a7d1c629e877d41e610d5a618c961be5
fredjuma007 <fredjuma8@gmail.com>
2022-09-22 09:50:33 -0700

    copy string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,7 +5,7 @@
  * @dest: destination to copy
  * @src: source to copy
  * @n: amount of places to copy
- * REturn: value of dest
+ * Return: value of dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
