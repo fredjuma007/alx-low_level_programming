f4922df2aedeb235ba733cc43fd56a24a82813af
fredjuma007 <fredjuma8@gmail.com>
2022-09-22 16:14:04 -0700

    upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * string_toupper - uppers a lower
+ * @s: character to make upper
+ * Return: return value of upper
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+	for (i = 0; str[i] != '\0'; i++)
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = str[i] - 32;
+	return (str);
+}
