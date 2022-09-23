bec1e705feafccbccf1386bf02d43d98d90606c0
fredjuma007 <fredjuma8@gmail.com>
2022-09-22 18:03:29 -0700

    moment of truth

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,3 +1,41 @@
 #include "main.h"
+/**
+ * _indexOf - returns bool of special char
+ * @a: character to return
+ * Return: true of false
+ * capARR is array capitalized
+ */
 
+int _indexOf(char a)
+{
+	int i;
+	char capArr[13] = {'\n', '\t', ' ', '.', ',', ';', ',', '!', '?', '(',
+')', '{', '}'};
 
+	for (i = 0; i < 13; i++)
+	{
+		if (capArr[i] == a)
+			return (1);
+	}
+	return (0);
+}
+/**
+ * cap_string - capitalize each word of the string
+ * @s: string
+ * Return: the string captilized
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (_indexOf(s[i]))
+			continue;
+		if (s[i] >= 'a' && s[i] <= 'z' && (_indexOf(s[i - 1]) || i == 0))
+			s[i] = s[i] - 32;
+
+	}
+	return (s);
+}	
