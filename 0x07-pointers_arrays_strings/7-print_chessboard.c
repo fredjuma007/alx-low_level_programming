72a19234d13580cce1c10e034c803b7e04970035
fredjuma007 <fredjuma8@gmail.com>
2022-09-26 15:40:23 -0700

    checkmate

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_chessboard - board(battlefield)
+ * @: print chess pieces
+ * Return: return bunch of stuff/chess crap
+ * lets play mate
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int king;
+	int queen;
+
+	for (king = 0; king < 8; king++)
+	{
+		for (queen = 0; queen < 8; queen++)
+		{
+			_putchar(a[king][queen]);
+		}
+		_putchar('\n');
+	}
+}
