689c22e98e34e4f34009bb09e235eda18da3436a
fredjuma007 <fredjuma8@gmail.com>
2022-09-26 11:10:46 -0700

    still not running

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,12 +1,10 @@
-#include "main.h"
 /**
- * _memcpy - copies from the source to the dest of the valvue
- * @dest: destination
- * @src: source
- * @n n amount to times
+ * _memcpy - copies from source to dest values in array
+ * @dest: destination to copy to
+ * @src: source to copy from
+ * @n: n amount of times
  * Return: return the string value
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
