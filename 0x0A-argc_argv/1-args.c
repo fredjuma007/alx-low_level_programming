4c452efb5cc783cefee899cff6fb09a604d08124
fredjuma007 <fredjuma8@gmail.com>
2022-09-30 10:57:18 -0700

    count arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+
+/**
+ * main -main function
+ * @argc: argumentc
+ * @argv: vector of arguments
+ * Return: 0
+ */
+
+int main(int arg, char *argv[])
+{
+	UNUSED(argv);
+	printf("%d\n", arg - 1);
+	return (0);
+}
