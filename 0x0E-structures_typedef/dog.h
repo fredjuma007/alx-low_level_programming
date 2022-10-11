206c05a3ccc349a78d4aee5f488d9778f9c4df3e
fredjuma007 <fredjuma8@gmail.com>
2022-10-11 09:41:52 -0700

    woof woof first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure of dog
+ * @name : name of dog (mine is Cujo)
+ * @age: age of dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
