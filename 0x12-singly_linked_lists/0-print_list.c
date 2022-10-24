bb1043b3dec960e5f1fe19c7e1ec75db57f896f3
fredjuma007 <fredjuma8@gmail.com>
2022-10-24 13:24:09 -0700

    fixing #0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
-
 /**
- * print_list - prints all element of a list
+ * print_list - prints a list
  * @h: head of list
  * Return: number of elements
  */
-
 size_t print_list(const list_t *h)
 {
 	int count = 0;
