97fb2e891474cac774256734889d65985ca28ce8
fredjuma007 <fredjuma8@gmail.com>
2022-10-26 15:08:51 -0700

    secont attempt at free

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -7,13 +7,16 @@
  */
 
 void free_listint(listint_t *head)
-{
-	listint_t *tmp;
-
-	while (head != NULL)
 	{
-		tmp = head;
-		free(head);
-		head = tmp->next;
+	listint_t *ptr;
+
+	if (head == NULL) /* account for no linked list */
+		return;
+
+	while (head != NULL) /* have ptr keep track of head node and free */
+	{
+		ptr = head;
+		head = head->next; /* move to next node while ptr frees prior */
+		free(ptr);
 	}
 }
