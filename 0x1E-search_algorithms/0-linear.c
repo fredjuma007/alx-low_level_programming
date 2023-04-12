e1186323cb85bb6b0a63fd2d4bc1291282592522
fredjuma007 <fredjuma007>
2023-04-12 19:51:40 +0200

    fixing betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,22 +1,20 @@
 #include "search_algos.h"
-
 /**
  * linear_search - searches linear
- * @array: array to
+ * @array: array to search
  * @size: size to go through
  * @value: value to search?
  * Return: int value
  */
-
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        printf("Value checked arra[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (i);
-    }
-    return (-1);
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
